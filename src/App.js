ebb993f612a19f93f072b1654e6915a9fe49c389
phfernandes14 <contato.phfernandes@gmail.com>
2020-10-26 09:47:29 -0300

    - Fetch na API e Exibição de Dados em Modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
+import Dialog from  './Dialog.js';
 
 function App() {
+
+  const [moedas, setMoedas] = useState(['USD'])
+  const [dados, setDados] = useState(Object)
+  const [exibirModal, setExibirModal] = useState(false)
+  
+  async function obtemCotacao(){
+    const url = `http://economia.awesomeapi.com.br/json/${moedas}-BRL`
+    await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);  
+
+           
+          const dados = {
+            venda: data[0].bid,
+            compra: data[0].ask,
+            codigo: data[0].code,
+            maxima: data[0].high,
+            minima: data[0].low,
+            nome: data[0].name,
+            variacao: data[0].pctChange,
+
+        }
+        setDados(dados)
+      
+        setExibirModal(true)
+
+        })
+        .catch(function (error) {
+          console.error('Houve um problema ao efetuar a requisição: ' + error.message);
+        });
+  }
   return (
+    <>
     <div className="App">
-      <p>Cotação-React</p>
-    </div>
+      <h1>Cotação - React</h1>
+      <div className="Menu">
+        <label for="select">Escolha uma Moeda: </label>
+          <select name="select" onChange={e => setMoedas(e.target.value)}>
+            <option value="USD">Dólar Comercial</option> 
+            <option value="USDT">Dólar Turismo</option>
+            <option value="CAD">Dólar Canadense</option>
+            <option value="AUD">Dólar Australiano</option>
+        </select>
+      </div>
+        <br></br>
+        <br></br>
+        <button onClick={obtemCotacao}>Obter Cotação</button>
+    </div> {
+      
+      exibirModal && 
+    
+    <Dialog dados={dados}  />
+
+
+
+
+}</>
   );
 }
 
---
+++ src/Dialog.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function TransitionsModal(dados, exibirModal) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+
+  const handleOpen = () => {
+    setOpen(true);
+    console.log('dados aqui:')
+    console.log(dados)
+ 
+
+    
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        react-spring
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+      <h2 id="transition-modal-title">{dados.dados.nome}</h2>
+            <p>Código:    <strong>{dados.dados.codigo}</strong></p>
+            <p>Máxima:   <strong>{dados.dados.maxima}</strong></p>
+            <p>Miníma:   <strong>{dados.dados.minima}</strong></p>
+            <p>Valor na Compra:   <strong>{dados.dados.compra}</strong> </p>
+            <p>Valor na Venda   <strong>{dados.dados.venda}</strong> </p>
+            <p>Variação:   <strong>{dados.dados.variacao}</strong> </p>
+            <small>https:..//github.com/phfernandes14/cotacao-react</small>
+            
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
